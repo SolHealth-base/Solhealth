85aa70efe7af5dc89aa86c7aa9d930689b582b12
TImothy Akanbi <knbtimothy@gmail.com>
2024-09-19 22:59:35 +0100

    chat component updated

--- sol-health/src/app/components/SendMessage.tsx
+++ sol-health/src/app/components/SendMessage.tsx
@@ -1,12 +1,10 @@
 'use client';
-// import { Button } from "@/components/ui/button"
-// import { Textarea } from "@/components/ui/textarea"
 
 const SendMessage = () => {
   return (
-    <div className="absolute bottom-0 left-0 w-full flex items-center">
-        <textarea className="h-5 text-black w-full" />
-        <button  className="ml-3">Send</button>
+    <div className="absolute bottom-0 left-0 w-full bg-slate-600 p-7 flex items-center">
+        <textarea className="h-10 p-1 text-black w-full" />
+        <button  className="ml-3 bg-black text-white p-5">Send</button>
     </div>
   )
 }
--- sol-health/src/app/components/ViewMessage.tsx
+++ sol-health/src/app/components/ViewMessage.tsx
@@ -9,9 +9,9 @@ const Message = ({ text, userId } : {
 
     // console.log(user?.uid)
     return (
-        <div id={userId} className={`mb-2 flex ${true? '' : 'flex-row-reverse'} justify-between`}>
+        <div id={userId} className={`mb-2 flex ${userId !== 'ai' ? '' : 'flex-row-reverse'} justify-between`}>
             <div className={'w-[20%]'}/>
-            <div className={`text-sm text-[#141414CC] font-normal leading-[22px] py-2.5 pr-2 px-[15px] ${true ? 'w-[fit-content] bg-[#F7F7F7] rounded-t-[12px] rounded-br-[12px]' : 'w-[fit-content] bg-[#FEEDE7] rounded-t-[12px] rounded-bl-[12px]'}`}>{text}</div>
+            <div className={`text-sm text-[#141414CC] font-normal leading-[22px] py-2.5 pr-2 px-[15px] ${userId !== 'ai'  ? 'w-[fit-content] bg-[#F7F7F7] rounded-t-[12px] rounded-br-[12px]' : 'w-[fit-content] bg-[#FEEDE7] rounded-t-[12px] rounded-bl-[12px]'}`}>{text}</div>
         </div>
     )
 }
@@ -21,10 +21,10 @@ const Messages = () => {
   return (
     <div className="mt-4">
         <Message text="hey" userId="L7egZf4DoZOYLeHLwN8ZAaKNg4o1"/>
-        <Message text="hey" userId="6"/>
+        <Message text="hey" userId="ai"/>
         <Message text="What's up?" userId="L7egZf4DoZOYLeHLwN8ZAaKNg4o1"/>
         <Message text="I've been trying to get hold of you since" userId="L7egZf4DoZOYLeHLwN8ZAaKNg4o1"/>
-        <Message text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque enim quaerat, quas excepturi temporibus consectetur maiores deleniti alias atque tempora dolor sed eveniet voluptatum magni qui veritatis culpa quo voluptatem illum, cumque reiciendis aut? Non fugit saepe incidunt a." userId="L7egZf4DoZOYLeHLwN8ZAaKNg4o1"/>
+        <Message text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque enim quaerat, quas excepturi temporibus consectetur maiores deleniti alias atque tempora dolor sed eveniet voluptatum magni qui veritatis culpa quo voluptatem illum, cumque reiciendis aut? Non fugit saepe incidunt a." userId="ai"/>
         <Message text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque enim quaerat, quas excepturi temporibus consectetur maiores deleniti alias atque tempora dolor sed eveniet voluptatum magni qui veritatis culpa quo voluptatem illum, cumque reiciendis aut? Non fugit saepe incidunt a." userId="L7egZf4DoZOYLeHLwN8ZAaKNg4o"/> 
         <Message text="I've been trying to get hold of you since" userId="L7egZf4DoZOYLeHLwN8ZAaKNg4o1"/>       
     </div>
