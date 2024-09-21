39c7369104e15a4c21c104c4b3890413e986b713
TImothy Akanbi <knbtimothy@gmail.com>
2024-09-21 11:17:34 +0100

    ts issues fixed

--- sol-health/src/app/components/Chat.tsx
+++ sol-health/src/app/components/Chat.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import SendMessage from "./SendMessage"
 import Messages from "./ViewMessage"
 
--- sol-health/src/app/page.tsx
+++ sol-health/src/app/page.tsx
@@ -1,14 +1,10 @@
 
 import Chat from "./components/Chat";
-import Check from "./components/Check";
-import Hey from "./components/Hey";
 
 export default function Home() {
   return (
     <div className="">
       <Chat />
-      {/* <Check? /> */}
-      {/* <Hey /> */}
     </div>
   );
 }
