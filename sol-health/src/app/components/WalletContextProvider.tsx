7fd10523d171f3b5d4401db41639b50b70dc0577
TImothy Akanbi <knbtimothy@gmail.com>
2024-09-29 15:21:00 +0100

    typescript is now a pain the ass

--- sol-health/src/app/components/WalletContextProvider.tsx
+++ sol-health/src/app/components/WalletContextProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { createContext, useContext, useMemo, ReactNode, useEffect } from 'react';
-import { ConnectionProvider, WalletProvider, useLocalStorage, useWallet as useSolanaWallet } from '@solana/wallet-adapter-react';
+import { ConnectionProvider, WalletProvider, useWallet as useSolanaWallet } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
